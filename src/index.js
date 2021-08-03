79adb3310cb3fc0ac184bc0ae224d694b69c8149
Renchester Ramos <renchesterjramos@gmail.com>
2021-08-03 16:31:46 +0800

    Add initialize page module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 // Imports
 
+import initPage from './initpage';
 import displayHome from './homepage';
 import displayAbout from './aboutpage';
 import displayMenu from './menupage';
@@ -9,14 +10,10 @@ import displayMenu from './menupage';
 // DOM Selectors
 
 const container = document.getElementById('content');
-// const navHome = document.querySelector('.nav-link.home');
-// const navMenu = document.querySelector('.nav-link.menu');
-// const navAbout = document.querySelector('.nav-link.about');
 
 // Event Listeners
 
-// window.addEventListener('load', displayHome);
-
+window.addEventListener('load', initPage);
 window.addEventListener('click', displayHome);
 window.addEventListener('click', displayMenu);
 window.addEventListener('click', displayAbout);
